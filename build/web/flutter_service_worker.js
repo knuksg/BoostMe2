3570904eb21c104c0abd897a3aa5d6e0f8d2835e
knuksg <knuksg@naver.com>
2024-05-14 10:07:11 +0900

    test

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -44,8 +44,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "ec790c4d0aed96139b97ce41da51dd1e
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2770fc00cdb8c5b037946c135dd11a50",
-"/": "2770fc00cdb8c5b037946c135dd11a50",
+"index.html": "b706461b9b00d0e38f1267499857c06d",
+"/": "b706461b9b00d0e38f1267499857c06d",
 "main.dart.js": "622e672c357ba2b862f8c1279bc1b2cd",
 "manifest.json": "16b5755a0d7f529047f89ec8da5521bd",
 "version.json": "342a6b9982b2d2607a55f14e7dd9879e"};
