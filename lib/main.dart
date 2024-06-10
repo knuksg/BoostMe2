4f1c3cbeab1c97cb9dbea0d6b0ed5bf37fc2f541
knuksg <knuksg@naver.com>
2024-06-10 16:33:45 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:boostme2/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -12,14 +10,6 @@ import 'presentation/viewmodels/auth_viewmodel.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Loading from a static string.
-  dotenv.testLoad(fileInput: '''FOO=foo
-BAR=bar
-''');
-
-// Loading from a file synchronously.
-  dotenv.testLoad(fileInput: File('assets/.env').readAsStringSync());
 
   await dotenv.load(fileName: "assets/.env");
 
