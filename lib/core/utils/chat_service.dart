a7761aeb7c0e396d2497c6d4f73db50f3995443e
knuksg <knuksg@naver.com>
2024-06-11 12:52:50 +0900

    test

--- lib/core/utils/chat_service.dart
+++ lib/core/utils/chat_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 
 class ChatService {
@@ -6,10 +7,23 @@ class ChatService {
 
   ChatService(this.serverUrl);
 
+  Future<String?> _getIdToken() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      return await user.getIdToken();
+    }
+    return null;
+  }
+
   Future<String> sendMessage(String message) async {
+    final idToken = await _getIdToken();
+    if (idToken == null) {
+      throw Exception('User not authenticated');
+    }
     final response = await http.post(
       Uri.parse('$serverUrl/chats'),
       headers: {
+        'Authorization': 'Bearer $idToken',
         'Content-Type': 'application/json',
       },
       body: jsonEncode({'message': message}),
